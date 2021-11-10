2c7276a035124c25ab2f407868e456eafdbaf50c
maylen aguiar <maylenaguiar.psi@gmail.com>
2021-11-09 23:10:09 -0300

    final

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,7 @@
 // Creo la clase Discos
 class Discos{
-    constructor(nombre, precio, cantidad) {
+    constructor(id, nombre, precio, cantidad) {
+        this.id = id;
         this.nombre = nombre;
         this.precio = precio;
         this.cantidad   = cantidad;
@@ -14,10 +15,10 @@ const productos = [];
 // Creo el array Carrito
 const carrito = [];
 //Creo mis objetos o productos. En este caso son discos de música
-const subibaja = new Discos('subibaja', 800, 22);
-const deOtroPlaneta = new Discos('de otro planeta', 1000, 34);
-const cocosLocos = new Discos ('cocos locos', 750, 30);
-const personaje = new Discos ('personaje', 900, 16);
+const subibaja = new Discos(1, 'subibaja', 800, 22);
+const deOtroPlaneta = new Discos(2, 'de otro planeta', 1000, 34);
+const cocosLocos = new Discos (3, 'cocos locos', 750, 30);
+const personaje = new Discos (4, 'personaje', 900, 16);
 subibaja.sumarIva ();
 deOtroPlaneta.sumarIva();
 cocosLocos.sumarIva();
@@ -27,152 +28,149 @@ productos.push({deOtroPlaneta});
 productos.push({cocosLocos});
 productos.push({personaje});
 //Creo variable para ver total del carrito
-let carritoTotal=0;
+let precioFinal = 0;
+//Funcion para renderizar carrito
+const mostrarCarrito =(elementos) => {
+    let lista = 0;
+    let precioFinal = 0;
+$('#contenedorProductos').empty();
+$('#js-precioTotal').empty();
+    for(const elemento of elementos){
+        lista += 1;
+        precioFinal += elemento.precio;
+        $('#contenedorProductos').append( `<div class= eliminarSubi>
+        <p class ="agregados"> Agregaste ${elemento.nombre} - a $ ${elemento.precio}</p> 
+        <button id='eliminar${elemento.id}${lista}'>Eliminar</button> 
+        </div>`
+        );
+        $(`#eliminar${elemento.id}${lista}`).on('click', ()=>{
+            let eliminarElemento = '';
+            eliminarElemento =productos.indexOf(elemento);
+            elementos.splice(eliminarElemento,1);
+            precioFinal -= elemento.precio;
+            mostrarCarrito(elementos);
+            if(elementos.length === 0){
+                $('#js-productosAgregados').css("display", "none");
+            }
+        })
+    }
+    $('#js-precioTotal').append(`El total es $${precioFinal}`);
+}
 //Botones para agregar discos al carrito
 let unBoton = document.getElementById("subi");
 let unBotonDiv = document.getElementById("popUpsubi");
 unBoton.addEventListener('click', () => {
-    carrito.push({subibaja});
-    carritoTotal += subibaja.precio;
+    carrito.push(subibaja);
+    guardarCarrito('carritoDeCompras',JSON.stringify(carrito));
+    precioFinal += subibaja.precio;
     if(carrito.length == 1){
-        $('#js-productosAgregados').append(
-                        `<div class="cart"> 
-                            <p> Agregaste Subibaja - Precio $ ${subibaja.precio}</p> 
-                            <button id="eliminar">Eliminar del carrito</button>
-                            <button id="boton">Finalizar Compra</button>
-                        </div>`   
-        );
-        let eliminar = document.getElementById("eliminar");
-        eliminar.addEventListener('click', () => {
-            carrito.splice({subibaja});
-            console.log(carrito);
-        })
+        $('#js-productosAgregados').css('display', 'block');
+        mostrarCarrito(carrito);
         let boton = document.getElementById("boton");
         boton.addEventListener('click', () => {
         let totalCompra = document.createElement("h3");
-        totalCompra.innerHTML= `<div class= carritoLleno> 
-                                <h3>El Total de tu compra es $ ${carritoTotal}</h3>
-                                <button id="finalizar">Finalizar</button></div>`
+        totalCompra.innerHTML= `<div class= "carritoLleno"> 
+                                <h3>Gracias por tu compra</h3>
+                                <button id="finalizar">Salir</button></div>`
         $(".cart").empty();
         document.body.appendChild (totalCompra);
-        let cerrar = document.getElementById('finalizar');
-        cerrar.addEventListener('click', () =>{
+        let salir = document.getElementById('finalizar');
+        salir.addEventListener('click', () =>{
         $('.carritoLleno').remove();
         })    
     })
 }
     else if(carrito.length >= 2){
-        $('.cart').prepend(
-        `<p> Agregaste Subibaja - Precio $ ${subibaja.precio}</p> 
-        <button id="eliminar">Eliminar del carrito</button>`
-        );
+        mostrarCarrito(carrito);
     }
   })      
 let esteBoton= document.getElementById('planeta');
 let otroDiv = document.getElementById('popUpPlaneta');
 esteBoton.addEventListener('click', () => {
-    carrito.push({deOtroPlaneta});
-    carritoTotal += deOtroPlaneta.precio;
+    carrito.push(deOtroPlaneta);
+    guardarCarrito('carritoDeCompras',JSON.stringify(carrito));
+    precioFinal += deOtroPlaneta.precio;
     if(carrito.length == 1){
-        $('#js-productosAgregados').append(
-                        `<div class="cart"> 
-                            <p> Agregaste De otro Planeta - Precio $ ${deOtroPlaneta.precio}</p> 
-                            <button id="eliminar">Eliminar del carrito</button>
-                            <button id="boton">Finalizar Compra</button>
-                            </div>`   
-        );
+        $('#js-productosAgregados').css('display', 'block');
+        mostrarCarrito(carrito);
         let boton = document.getElementById("boton");
         boton.addEventListener('click', () => {
         let totalCompra = document.createElement("h3");
         totalCompra.innerHTML= `<div class= carritoLleno> 
-                                <h3>El Total de tu compra es $ ${carritoTotal}</h3>
-                                <button id="finalizar">Finalizar</button></div>`
+                                <h3>Gracias por tu compra</h3>
+                                <button id="finalizar">Salir</button></div>`
         $(".cart").empty();
         document.body.appendChild (totalCompra);
-        let cerrar = document.getElementById('finalizar');
-        cerrar.addEventListener('click', () =>{
+        let salir = document.getElementById('finalizar');
+        salir.addEventListener('click', () =>{
         $('.carritoLleno').remove();
         })    
     })
     }
     else if(carrito.length >= 2){
-        $('.cart').prepend(
-        `<p> Agregaste De otro Planeta - Precio $ ${deOtroPlaneta.precio}</p> 
-        <button id="eliminar">Eliminar del carrito</button>`
-        );
+        mostrarCarrito(carrito);
     }
 })
 let tercerBoton= document.getElementById('personaje');
 let tercerDiv = document.getElementById('popUpPersonaje');
 tercerBoton.addEventListener('click', () => {
-    carrito.push({personaje});
-    carritoTotal += personaje.precio;
+    carrito.push(personaje);
+    guardarCarrito('carritoDeCompras',JSON.stringify(carrito));
+    precioFinal += personaje.precio;
     if(carrito.length == 1){
-        $('#js-productosAgregados').append(
-                        `<div class="cart"> 
-                            <p> Agregaste Personaje - Precio $ ${personaje.precio}</p> 
-                            <button id="eliminar">Eliminar del carrito</button>
-                            <button id="boton">Finalizar Compra</button>
-                        </div>`   
-        );
+        $('#js-productosAgregados').css('display', 'block');
+        mostrarCarrito(carrito);
         let boton = document.getElementById("boton");
         boton.addEventListener('click', () => {
         let totalCompra = document.createElement("h3");
         totalCompra.innerHTML= `<div class= carritoLleno> 
-                                <h3>El Total de tu compra es $ ${carritoTotal}</h3>
-                                <button id="finalizar">Finalizar</button></div>`
+                                <h3>Gracias por tu compra</h3>
+                                <button id="finalizar">Salir</button></div>`
         $(".cart").empty();
         document.body.appendChild (totalCompra);
-        let cerrar = document.getElementById('finalizar');
-        cerrar.addEventListener('click', () =>{
+        let salir = document.getElementById('finalizar');
+        salir.addEventListener('click', () =>{
+        $('.carritoLleno').remove();
+        })
+    })
+    }
+    else if(carrito.length >= 2){
+        mostrarCarrito(carrito);
+    }
+    })
+        let cuartoBoton= document.getElementById('locos');
+        let cuartoDiv = document.getElementById('popUpLocos');
+        cuartoBoton.addEventListener('click', () => {
+        carrito.push(cocosLocos);
+        guardarCarrito('carritoDeCompras',JSON.stringify(carrito));
+        precioFinal += cocosLocos.precio;
+    if(carrito.length == 1){
+        $('#js-productosAgregados').css('display', 'block');
+        mostrarCarrito(carrito);
+        let boton = document.getElementById("boton");
+        boton.addEventListener('click', () => {
+        let totalCompra = document.createElement("h3");
+        totalCompra.innerHTML= `<div class= carritoLleno> 
+                                <h3>Gracias por tu compra</h3>
+                                <button id="finalizar">Salir</button></div>`
+        $(".cart").empty();
+        document.body.appendChild (totalCompra);
+        let salir = document.getElementById('finalizar');
+        salir.addEventListener('click', () =>{
         $('.carritoLleno').remove();
         })    
     })
     }
     else if(carrito.length >= 2){
-        $('.cart').prepend(
-        `<p> Agregaste Personaje - Precio $ ${personaje.precio}</p>
-        <button id="eliminar">Eliminar del carrito</button>`
-        );
-    }
-})
-let cuartoBoton= document.getElementById('locos');
-let cuartoDiv = document.getElementById('popUpLocos');
-cuartoBoton.addEventListener('click', () => {
-    carrito.push({cocosLocos});
-    carritoTotal += cocosLocos.precio;
-    if(carrito.length == 1){
-        $('#js-productosAgregados').append(
-                        `<div class="cart"> 
-                            <p> Agregaste Cocos Locos - Precio $ ${cocosLocos.precio}</p>  
-                            <button id="eliminar">Eliminar del carrito</button>
-                            <button id="boton">Finalizar Compra</button>
-                        </div>`   
-        );
-        let boton = document.getElementById("boton");
-        boton.addEventListener('click', () => {
-        let totalCompra = document.createElement("h3");
-        totalCompra.innerHTML= `<div class= carritoLleno> 
-                                <h3>El Total de tu compra es $ ${carritoTotal}</h3>
-                                <button id="finalizar">Finalizar</button></div>`
-        $(".cart").empty();
-        document.body.appendChild (totalCompra);
-        let cerrar = document.getElementById('finalizar');
-        cerrar.addEventListener('click', () =>{
-        $('.carritoLleno').remove();
-        })    
-    })
-    }
-    else if(carrito.length >= 2){
-        $('.cart').prepend(
-        `<p> Agregaste Cocos Locos - Precio $ ${cocosLocos.precio}</p> 
-        <button id="eliminar">Eliminar del carrito</button>`
-        );
+        mostrarCarrito(carrito);
     }  
 })
-//Objetos guardados en localStorage
+//Productos guardados en localStorage
 const guardarLocal = (productos,precio) => {localStorage.setItem(productos,precio)};
 guardarLocal('listaProductos',JSON.stringify(productos));
+//Carrito de compras guardados en localStorage
+const guardarCarrito = (carritoComprado,carrito) => {localStorage.setItem(carritoComprado,carrito)};
+guardarCarrito('carritoDeCompras',JSON.stringify(carritoComprado, carrito));
 //Uso de Api para formulario simulado
 $(document).ready(function () {
     const APIURL = "http://jsonplaceholder.typicode.com/posts";
